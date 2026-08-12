ce7672a6fa3883a0e7aeb26e14c6d8c542370c17
Danish-Spots <github.municipal976@passmail.net>
2026-08-12 20:48:58 +0200

    Moved all oold files to .old
    Deleted old flakek lock
    
    Created new minimal flake loock
    
    Copied configuration and harware-configuration files to .system

--- flake.nix
+++ flake.nix
@@ -1,74 +1,17 @@
 {
-  description = ''
-    For questions just DM me on X: https://twitter.com/@m3tam3re
-    There is also some NIXOS content on my YT channel: https://www.youtube.com/@m3tam3re
-
-    One of the best ways to learn NIXOS is to read other peoples configurations. I have personally learned a lot from Gabriel Fontes configs:
-    https://github.com/Misterio77/nix-starter-configs
-    https://github.com/Misterio77/nix-config
-
-    Please also check out the starter configs mentioned above.
-  '';
+  description = "NixOS configuration";
 
   inputs = {
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
-    walker.url = "github:abenz1267/walker";
-
-    # Optional, if you intend to follow nvf's obsidian-nvim input
-    # you must also add it as a flake input.
-    # obsidian-nvim.url = "github:epwalsh/obsidian.nvim";
-
-    # Required, nvf works best and only directly supports flakes
-    # nvf = {
-    #   url = "github:NotAShelf/nvf";
-    #   # You can override the input nixpkgs to follow your system's
-    #   # instance of nixpkgs. This is safe to do as nvf does not depend
-    #   # on a binary cache.
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    #   # Optionally, you can also override individual plugins
-    #   # for example:
-    #   # inputs.obsidian-nvim.follows = "obsidian-nvim"; # <- this will use the obsidian-nvim from your inputs
-    # };
-
-    nvf.url = "github:notashelf/nvf";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    home-manager,
-    nixpkgs,
-    nvf,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = [
-      "x86_64-linux"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    packages =
-      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
-    overlays = import ./overlays {inherit inputs;};
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./hosts/nixos];
-      };
-    };
-    homeConfigurations = {
-      "justin@nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        extraSpecialArgs = {inherit inputs outputs;};
-        modules = [
-          nvf.homeManagerModules.default
-          ./home/justin/nixos.nix
-        ];
-      };
+  outputs = { nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      modules = [
+        ./system/configuration.nix
+      ];
     };
   };
 }

bd51400a2b515df470d7b8f9ee8808a8e6603d9e
Danish-Spots <github.municipal976@passmail.net>
2026-08-15 18:06:56 +0200

    Added hyprland

---
+++ features/hyprland/default.nix
@@ -0,0 +1,21 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.features.hyprland;
+in
+{
+  options.features.hyprland = {
+    enable = lib.mkEnableOption "Enable Hyprland with default configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+  };
+
+  home-manager.sharedModules = [
+    ./home.nix
+  ];
+}
